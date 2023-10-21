19c5fff783143c81fefe1bd5450381043a410fa1
Jashanpreet Singh <72213670+JashanpreetSinghp09@users.noreply.github.com>
2023-10-21 19:56:33 -0400

    error fixed

--- PHRApplication/app/build.gradle.kts
+++ PHRApplication/app/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth-ktx:22.1.2")
     implementation("com.google.firebase:firebase-database:20.2.2")
+    implementation ("org.web3j:core:5.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
