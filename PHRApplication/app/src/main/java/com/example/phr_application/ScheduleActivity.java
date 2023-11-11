c7c16461ee62afd6ef46a02987ca28d4ebb96bfd
Jashanpreet Singh <72213670+JashanpreetSinghp09@users.noreply.github.com>
2023-11-10 22:28:05 -0500

    Schdeule_activity backend and User interface completed PART1
    sch activity user interface designed partially, backend included

--- PHRApplication/app/src/main/java/com/example/phr_application/ScheduleActivity.java
+++ PHRApplication/app/src/main/java/com/example/phr_application/ScheduleActivity.java
@@ -3,6 +3,11 @@ package com.example.phr_application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class ScheduleActivity extends AppCompatActivity {
 
@@ -10,5 +15,54 @@ public class ScheduleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
+
+        // array of TextView IDs for dates and days
+
+        int[] dateTextViewIds = {R.id.textView_date1, R.id.textView_date2, R.id.textView_date3, R.id.textView_date4, R.id.textView_date5, R.id.textView_date6, R.id.textView_date7, R.id.textView_date8, R.id.textView_date9, R.id.textView_date10};
+        int[] dayTextViewIds = {R.id.textView_day1, R.id.textView_day2, R.id.textView_day3, R.id.textView_day4, R.id.textView_day5, R.id.textView_day6, R.id.textView_day7, R.id.textView_day8, R.id.textView_day9, R.id.textView_day10};
+
+
+        // Get the current date and day
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd", Locale.getDefault());
+        SimpleDateFormat dayOfWeekFormat = new SimpleDateFormat("EEE", Locale.getDefault());
+
+        for(int i = 0; i < dateTextViewIds.length; i++){
+
+            TextView textViewDate = findViewById(dateTextViewIds[i]);
+            TextView textViewDay = findViewById(dayTextViewIds[i]);
+
+            // Set the current date and day to the TextViews
+            textViewDate.setText(dateFormat.format(calendar.getTime()));
+            textViewDay.setText(dayOfWeekFormat.format(calendar.getTime()));
+
+            // Set the current date and day to the TextViews
+            textViewDate.setText(dateFormat.format(calendar.getTime()));
+            textViewDay.setText(dayOfWeekFormat.format(calendar.getTime()));
+
+            // increment the date for next day
+            calendar.add(Calendar.DAY_OF_MONTH,1);
+
+        }
+
+//        // Initialize the tv variable with tv objects
+//
+//
+//        TextView textViewDate1 = findViewById(R.id.textView_date1);
+//        TextView textViewDay1 = findViewById(R.id.textView_day1);
+//        TextView textViewDate2 = findViewById(R.id.textView_date2);
+//        TextView textViewDay2 = findViewById(R.id.textView_day2);
+//
+//        // Set the current date and day to the TextViews
+//        textViewDate1.setText(dateFormat.format(calendar.getTime()));
+//        textViewDay1.setText(dayOfWeekFormat.format(calendar.getTime()));
+//
+//        // increment the date for next day
+//        calendar.add(Calendar.DAY_OF_MONTH,1);
+//
+//        // set the day and date for next day(s)
+//        textViewDate2.setText(dateFormat.format(calendar.getTime()));
+//        textViewDay2.setText(dayOfWeekFormat.format(calendar.getTime()));
+
     }
 }
