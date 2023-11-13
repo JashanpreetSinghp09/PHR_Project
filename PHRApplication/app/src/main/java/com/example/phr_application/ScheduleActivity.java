754670195e2b15382b86da3cec9c3d7ecc2a51ee
farhan0474 <farha11a@uwindsor.ca>
2023-11-12 20:03:45 -0500

    Modified Schedule Activity

--- PHRApplication/app/src/main/java/com/example/phr_application/ScheduleActivity.java
+++ PHRApplication/app/src/main/java/com/example/phr_application/ScheduleActivity.java
@@ -21,12 +21,31 @@ public class ScheduleActivity extends AppCompatActivity {
         int[] dateTextViewIds = {R.id.textView_date1, R.id.textView_date2, R.id.textView_date3, R.id.textView_date4, R.id.textView_date5, R.id.textView_date6, R.id.textView_date7, R.id.textView_date8, R.id.textView_date9, R.id.textView_date10};
         int[] dayTextViewIds = {R.id.textView_day1, R.id.textView_day2, R.id.textView_day3, R.id.textView_day4, R.id.textView_day5, R.id.textView_day6, R.id.textView_day7, R.id.textView_day8, R.id.textView_day9, R.id.textView_day10};
 
+        //array of TextView IDs for dates and days in appointment list
+
+        int[] dateTextViewIds2 = {R.id.text_date1, R.id.text_date2, R.id.text_date3};
+        int[] dayTextViewIds2 = {R.id.text_day1, R.id.text_day2, R.id.text_day3};
+
+
 
         // Get the current date and day
         Calendar calendar = Calendar.getInstance();
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd", Locale.getDefault());
         SimpleDateFormat dayOfWeekFormat = new SimpleDateFormat("EEE", Locale.getDefault());
 
+
+        //Changing date of appointment ones
+        for(int i = 0; i < dateTextViewIds2.length; i++){
+
+            TextView textViewDate = findViewById(dateTextViewIds2[i]);
+            TextView textViewDay = findViewById(dayTextViewIds2[i]);
+
+            // Set the current date and day to the TextViews
+            textViewDate.setText(dateFormat.format(calendar.getTime()));
+            textViewDay.setText(dayOfWeekFormat.format(calendar.getTime()));
+        }
+
+        //Changing date of schedule list
         for(int i = 0; i < dateTextViewIds.length; i++){
 
             TextView textViewDate = findViewById(dateTextViewIds[i]);
@@ -36,15 +55,9 @@ public class ScheduleActivity extends AppCompatActivity {
             textViewDate.setText(dateFormat.format(calendar.getTime()));
             textViewDay.setText(dayOfWeekFormat.format(calendar.getTime()));
 
-            // Set the current date and day to the TextViews
-            textViewDate.setText(dateFormat.format(calendar.getTime()));
-            textViewDay.setText(dayOfWeekFormat.format(calendar.getTime()));
-
             // increment the date for next day
             calendar.add(Calendar.DAY_OF_MONTH,1);
-
         }
-
 //        // Initialize the tv variable with tv objects
 //
 //
