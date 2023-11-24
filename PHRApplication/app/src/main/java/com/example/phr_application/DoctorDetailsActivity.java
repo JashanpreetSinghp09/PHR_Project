e06ff671ddad4e99773597205f549055745067b3
Jashanpreet Singh <72213670+JashanpreetSinghp09@users.noreply.github.com>
2023-11-23 20:11:30 -0500

    design doctor details page v1.0

---
+++ PHRApplication/app/src/main/java/com/example/phr_application/DoctorDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.phr_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DoctorDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_details);
+    }
+}
