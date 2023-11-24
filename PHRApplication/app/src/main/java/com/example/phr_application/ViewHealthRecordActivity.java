0aa3ea62a9f64e7b937a04407a0a47b78159d4c7
farhan0474 <farha11a@uwindsor.ca>
2023-11-23 19:40:49 -0500

    ViewHealthRecordActivity modified
    added multiple details to the activity view

--- PHRApplication/app/src/main/java/com/example/phr_application/ViewHealthRecordActivity.java
+++ PHRApplication/app/src/main/java/com/example/phr_application/ViewHealthRecordActivity.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class ViewHealthRecordActivity extends AppCompatActivity {
 
-    private TextView viewRecord;
+    private TextView visiAllergy, visiCurrentMedication, visiImmunization, visiCurrentSymptoms, visiDiagnosis, visiTreatmentPlan;
+    private TextView allergy, currentMedication, immunization, currentSymptoms, diagnosis, treatmentPlan;
     private Button viewButton;
     private WalletManager walletManager;
 
@@ -19,8 +21,31 @@ public class ViewHealthRecordActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_health_record);
 
-        viewRecord = findViewById(R.id.viewingHealth);
+        visiAllergy = findViewById(R.id.textView16);
+        visiCurrentMedication = findViewById(R.id.textView28);
+        visiImmunization = findViewById(R.id.textView30);
+        visiCurrentSymptoms = findViewById(R.id.textView32);
+        visiDiagnosis = findViewById(R.id.textView34);
+        visiTreatmentPlan = findViewById(R.id.textView36);
+
+        visiAllergy.setVisibility(View.INVISIBLE);
+        visiCurrentMedication.setVisibility(View.INVISIBLE);
+        visiImmunization.setVisibility(View.INVISIBLE);
+        visiCurrentSymptoms.setVisibility(View.INVISIBLE);
+        visiDiagnosis.setVisibility(View.INVISIBLE);
+        visiTreatmentPlan.setVisibility(View.INVISIBLE);
+
+
+        allergy = findViewById(R.id.viewingAllergy);
+        currentMedication = findViewById(R.id.viewingMedication);
+        immunization = findViewById(R.id.viewingImmunization);
+        currentSymptoms = findViewById(R.id.viewingSymptoms);
+        diagnosis = findViewById(R.id.viewingDiagnosis);
+        treatmentPlan = findViewById(R.id.viewingTreatment);
+
+
         viewButton = findViewById(R.id.viewingButton);
+
         walletManager = WalletManager.getInstance(this);
 
         viewButton.setOnClickListener(new View.OnClickListener() {
@@ -31,7 +56,8 @@ public class ViewHealthRecordActivity extends AppCompatActivity {
                     @Override
                     public void onRetrieveSuccess(String result) {
 
-                        viewRecord.setText(result);
+                        setInputData(result, allergy, currentMedication, immunization, currentSymptoms, diagnosis, treatmentPlan);
+                        setVisibility(visiAllergy, visiCurrentMedication, visiImmunization, visiCurrentSymptoms, visiDiagnosis, visiTreatmentPlan);
                     }
 
                     @Override
@@ -42,5 +68,22 @@ public class ViewHealthRecordActivity extends AppCompatActivity {
                 });
             }
         });
+    }
+
+    void setInputData(String inputData, TextView... textViews) {
+
+        String[] substrings = inputData.split("\n");
+
+        // Iterate over the textViews and set the text for each one
+        for (int i = 0; i < Math.min(substrings.length, textViews.length); i++) {
+            textViews[i].setText(substrings[i]);
+        }
+    }
+
+    void setVisibility(TextView... textViews) {
+
+        for (TextView textView : textViews) {
+            textView.setVisibility(View.VISIBLE);
+        }
     }
 }
