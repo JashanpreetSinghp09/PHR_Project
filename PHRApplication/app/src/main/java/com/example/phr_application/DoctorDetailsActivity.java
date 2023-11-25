41014750c59f9a230477c32ccdb69dbb1416989c
farhan0474 <farha11a@uwindsor.ca>
2023-11-24 23:15:36 -0500

    Doctor Details changes
    Added extra firestore db input

--- PHRApplication/app/src/main/java/com/example/phr_application/DoctorDetailsActivity.java
+++ PHRApplication/app/src/main/java/com/example/phr_application/DoctorDetailsActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,7 +20,8 @@ import com.google.firebase.database.ValueEventListener;
 public class DoctorDetailsActivity extends AppCompatActivity {
 
     SharedPreferences sharedPreferences;
-    TextView drName, drSpecialty, drAboutSection, drAvailability;
+    TextView drName, drSpecialty, drAboutSection, drAvailability, drPatientNo, drExperience, drRating;
+    ImageView backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,13 +31,27 @@ public class DoctorDetailsActivity extends AppCompatActivity {
         drSpecialty = findViewById(R.id.tv_docRole);
         drAboutSection = findViewById(R.id.tv_about);
         drAvailability = findViewById(R.id.tv_time);
+        drPatientNo = findViewById(R.id.tv_noOfPatients);
+        drExperience = findViewById(R.id.tv_experience);
+        drRating = findViewById(R.id.tv_rating);
+        backButton = findViewById(R.id.imageView_back);
 
-        sharedPreferences = getSharedPreferences("Details", MODE_PRIVATE);
-        String pr = sharedPreferences.getString("DoctorName", "");
-        System.out.println(pr);
         //Initializing the Firebase realtime database
         FirebaseApp.initializeApp(DoctorDetailsActivity.this);
+
+        //Taking doctor identity
+        sharedPreferences = getSharedPreferences("Details", MODE_PRIVATE);
+
+        //Setting doctor details
         setupDoctorDetails(sharedPreferences.getString("DoctorName", ""));
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle the back button click
+                finish();
+            }
+        });
 
 
     }
@@ -54,6 +70,9 @@ public class DoctorDetailsActivity extends AppCompatActivity {
                     drSpecialty.setText(dataSnapshot.child("specialty").getValue(String.class));
                     drAvailability.setText(dataSnapshot.child("time").getValue(String.class));
                     drAboutSection.setText(dataSnapshot.child("about").getValue(String.class));
+                    drPatientNo.setText(dataSnapshot.child("noOfPatients").getValue(String.class));
+                    drExperience.setText(dataSnapshot.child("experience").getValue(String.class));
+                    drRating.setText(dataSnapshot.child("rating").getValue(Double.class).toString());
 
                 }
             }
